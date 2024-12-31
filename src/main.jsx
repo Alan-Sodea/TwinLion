6f06c1d3b4ff9b116114fe130b4efa661395b711
Alan-Sodea <konzuosodea@gmail.com>
2024-12-31 17:52:02 +0100

    final1

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Gallery from './Gallery.jsx'
-=======
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
->>>>>>> ab5a1519b88bb611c90f2fb56451b0c6002ea95a
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<<<<<<< HEAD
-    {/* <App /> */}
     <BrowserRouter>
     
       <Routes>
@@ -25,8 +22,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 );
-=======
-    <App />
-  </StrictMode>,
-)
->>>>>>> ab5a1519b88bb611c90f2fb56451b0c6002ea95a
